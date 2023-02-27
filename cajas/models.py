d13c741856c3f287d868ea6c7cdbc2a622ae9cd6
myrosandrade <myrosandrade89@gmail.com>
2023-02-26 19:00:09 -0800

    cajas model updated

--- cajas/models.py
+++ cajas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Caja(models.Model):
+    nombre = models.CharField(max_length=100)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Cajas'
+        ordering = ['nombre']
