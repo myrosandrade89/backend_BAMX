bb5a25de7bb3ed36927072731b99f641dc7a8635
myrosandrade <myrosandrade89@gmail.com>
2023-02-26 19:08:25 -0800

    cajas views updated

--- cajas/views.py
+++ cajas/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from cajas.models import Caja
+from cajas.serializers import CajaSerializer
+from rest_framework import viewsets, permissions
+
 
 # Create your views here.
+
+class CajaViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows cajas to be viewed or edited
+    """
+    queryset = Caja.objects.all().order_by('nombre')
+    serializer_class = CajaSerializer
+    permission_classes = [permissions.IsAuthenticated]
