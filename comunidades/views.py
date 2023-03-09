5b60eab14c04e8bdb953dd5a5425b6b314387e1f
myrosandrade <myrosandrade89@gmail.com>
2023-03-09 01:43:11 -0800

    permissions added

--- comunidades/views.py
+++ comunidades/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets, permissions
 
 # Create your views here.
 
+# Viewset that allows comunidades to be viewed by any user and edited by admins only
 class ComunidadViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows comunidades to be viewed or edited
@@ -12,3 +13,8 @@ class ComunidadViewSet(viewsets.ModelViewSet):
     queryset = Comunidad.objects.all().order_by('nombre')
     serializer_class = ComunidadSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            self.permission_classes = [permissions.IsAdminUser]
+        return super(self.__class__, self).get_permissions()
