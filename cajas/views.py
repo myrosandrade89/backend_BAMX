5e5b7c9da335c5f6839c8cb2271f47fca50e814a
myrosandrade <myrosandrade89@gmail.com>
2023-03-09 01:42:49 -0800

    libraries fixed

--- cajas/views.py
+++ cajas/views.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from cajas.models import Caja
 from cajas.serializers import CajaSerializer, CajaIDSerializer
-from rest_framework import viewsets, permissions
 from turnos.models import Turno
 
 
