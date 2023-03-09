701fef87478d55668deee80d159010590ea13565
myrosandrade <myrosandrade89@gmail.com>
2023-03-09 01:42:24 -0800

    my-user url added

--- bamx_backend/urls.py
+++ bamx_backend/urls.py
@@ -4,6 +4,7 @@ from comunidades.views import ComunidadViewSet
 from turnos.views import TurnoViewSet
 from cajas.views import CajaViewSet
 from users.views import UserViewSet
+from users.views import CurrentUserView
 from link.views import LinkViewSet
 
 
@@ -18,6 +19,6 @@ router.register(r'link', LinkViewSet,basename="link")
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
     path('', include(router.urls)),
+    path('users/my', CurrentUserView.as_view()),
     path('api-auth/', include('rest_framework.urls')),
-
 ]
