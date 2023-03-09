97cbedb27e81555b5f3c36a72d3bc07321d32e47
myrosandrade <myrosandrade89@gmail.com>
2023-03-09 01:44:03 -0800

    my-user endpoint added

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, views
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from django.contrib.auth import get_user_model as User
 from users.serializers import UserSerializer
 
@@ -6,3 +8,11 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User().objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+
+# View for getting the current user
+class CurrentUserView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
