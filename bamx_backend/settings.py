6ea1d83064c57e2d8f56f4edbbe516d7d5f95abd
myrosandrade <myrosandrade89@gmail.com>
2023-03-10 14:31:12 -0800

    cors added

--- bamx_backend/settings.py
+++ bamx_backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'cajas',
     'users',
     'link',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'bamx_backend.urls'
@@ -134,3 +137,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication', 
     ],
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
